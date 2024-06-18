402e86d86e44d866706ed11117f761ab6b788748
FastDev <101209411+Student-FastDev@users.noreply.github.com>
2024-06-18 17:33:47 +0200

    Added code.

---
+++ OIJ/X/Regal (regal).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    vector<int> ballCounter;
+    
+    for(int i = 0; i < N; i++) {
+        string A;
+        cin >> A;
+        
+        ballCounter.push_back(A.length());
+    }
+    
+    sort(ballCounter.begin(), ballCounter.end());
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < ballCounter[i]; j++) {
+            cout << "O";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
