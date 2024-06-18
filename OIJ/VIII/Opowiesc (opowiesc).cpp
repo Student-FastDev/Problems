16e0b664d2a9a33fb705adbc96f2bb115a9b9c64
FastDev <101209411+Student-FastDev@users.noreply.github.com>
2024-06-18 17:34:46 +0200

    Added code.

---
+++ OIJ/VIII/Opowiesc (opowiesc).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool countArray[256];
+
+int main()
+{
+    int N;
+    string S;
+    cin >> N >> S;
+
+    int countSentence = 0;
+
+    for(int i = 0; i < N; i++) {
+        if(S[i] >= 'A' && S[i] <= 'Z') {
+            countArray[S[i]] = true;
+            
+            if(countArray[S[i] + 32] == true) {
+                countSentence++;
+            }
+            
+        }
+        else if(S[i] >= 'a' && S[i] <= 'z') {
+            countArray[S[i]] = true;
+            
+            if(countArray[S[i] - 32] == true) {
+                countSentence++;
+            }
+        }
+    }
+    
+    cout << countSentence << endl;
+
+    return 0;
+}
