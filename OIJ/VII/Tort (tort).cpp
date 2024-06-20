b5a07b65056ce9b3a5e616953563b7a808039907
FastDev <101209411+Student-FastDev@users.noreply.github.com>
2024-06-20 21:03:45 +0200

    Added code.

---
+++ OIJ/VII/Tort (tort).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, d = 0;
+    cin >> n;
+    int tab1[n];
+    int tab2[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> tab1[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> tab2[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (tab1[i] > tab2[i]) {
+            d++;
+        }
+    }
+    cout << d++;
+}
