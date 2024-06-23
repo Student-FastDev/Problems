524512769f6b2ba6038814b9ede39c22a103aeb7
FastDev <101209411+Student-FastDev@users.noreply.github.com>
2024-06-23 22:31:55 +0200

    Added code.

---
+++ OIJ/X/Mistrzostwa (mistrzostwa).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    cout.tie(0);
+    
+    string text;
+    cin >> text;
+    
+    int suma = 0;
+    
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] != '?') {
+            suma = suma + text[i] - '0';
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if ((suma + i) % 3 == 0) {
+            cout << i;
+            return 0;
+        }
+    }
+    return 0;
+}
