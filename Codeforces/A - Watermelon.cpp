0ddec3a9bc57bb8991e85627f02be15a73103872
Dawid Kot <101209411+Student-FastDev@users.noreply.github.com>
2024-10-19 13:32:56 +0200

    Added code.

---
+++ Codeforces/A - Watermelon.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int N;
+    cin >> N;
+    
+    if(N == 2) {
+        cout << "NO" << endl;
+        return 0;
+    }
+    
+    if(N % 2 == 0) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+ 
+    return 0;
+}
