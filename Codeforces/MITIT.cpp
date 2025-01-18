acfb30bccd38e3632e102cebc5bd070d8897ff9d
Dawid Kot <101209411+Student-FastDev@users.noreply.github.com>
2025-01-18 14:16:28 +0100

    Added code.

---
+++ Codeforces/MITIT.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+auto operator<<(auto&o,auto p)->decltype(p.first,o){return o<<'('<<p.first<<", "<<p.second <<')';}
+auto operator<<(auto&o,auto x)->decltype(x.end(),o){o<<'{';int i=2;for(auto e:x)o<<(", ")+i<<e,i=0;return o<<'}';}
+#define LOG(x...)cerr<<"["#x"]: ",[](auto...$){((cerr<<$<<"; "),...)<<endl;}(x);
+#else
+#define LOG(x...)(void)0
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x)  (x).begin(),(x).end()
+#define endl    '\n'
+#define size(x)  x.size()
+
+const ll INF = 9223372036854775806;
+const ll MAX_N = 1e9+1;
+const ll MOD = 1e9+7; // 998244353
+
+void solveTestCase() {
+    string S;
+    cin >> S;
+    int L = size(S);
+    bool foundValue = false;
+    for(int B = 1; B <= (L-1) / 2; ++B){
+        if(L - 2*B < 1){
+            continue;
+        }
+        string B1 = S.substr(L - 2*B, B);
+        string B2 = S.substr(L - B, B);
+        if(B1 == B2){
+            foundValue = true;
+            break;
+        }
+    }
+    if(foundValue){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        solveTestCase();
+    }
+    return 0;
+}
