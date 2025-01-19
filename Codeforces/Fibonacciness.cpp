e278a1b2b0031ae9e52cefc50baf149b1a9ce7e1
Dawid Kot <101209411+Student-FastDev@users.noreply.github.com>
2025-01-19 22:31:23 +0100

    Added code.

---
+++ Codeforces/Fibonacciness.cpp
@@ -0,0 +1,66 @@
+// Catling
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+auto operator<<(auto&o,auto p)->decltype(p.first,o){return o<<'('<<p.first<<", "<<p.second <<')';}
+auto operator<<(auto&o,auto x)->decltype(x.end(),o){o<<'{';int i=2;for(auto e:x)o<<(", ")+i<<e,i=0;return o<<'}';}
+#define LOG(x...)cerr<<"["#x"]: ",[](auto...$){((cerr<<$<<"; "),...)<<endl;}(x);
+#else
+#define LOG(x...)(void)0
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x)  (x).begin(),(x).end()
+#define endl    '\n'
+#define size(x)  x.size()
+
+const ll INF = 9223372036854775806;
+const ll MAX_N = 1e9+1;
+const ll MOD = 1e9+7;
+
+void solveTestCase() {
+    int A, B, D, E;
+    cin >> A >> B >> D >> E;
+    
+    vector<long long> candidates;
+    candidates.push_back((long long)A + B);
+    candidates.push_back((long long)D - B);
+    candidates.push_back((long long)E - D);
+
+    int maxCount = 0;
+
+    for(auto C : candidates){
+        int count = 0;
+        if(C == (long long)A + B){
+            count++;
+        }
+        if(D == (long long)B + C){
+            count++;
+        }
+        if(E == (long long)C + D){
+            count++;
+        }
+        if(count > maxCount){
+            maxCount = count;
+        }
+    }
+    
+    cout << maxCount << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        solveTestCase();
+    }
+    return 0;
+}
