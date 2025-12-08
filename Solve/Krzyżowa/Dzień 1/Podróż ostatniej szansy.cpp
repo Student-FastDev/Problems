34f37db021b816ed7f3d42792dc36f562aae07a7
Dawid Kot <101209411+Student-FastDev@users.noreply.github.com>
2025-12-08 17:13:10 +0100

    Added code.

---
+++ Solve/Krzyżowa/Dzień 1/Podróż ostatniej szansy.cpp
@@ -0,0 +1,70 @@
+// Catling
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+auto operator<<(auto&o,auto p)->decltype(p.first,o){return o<<'('<<p.first<<", "<<p.second <<')';}
+auto operator<<(auto&o,auto x)->decltype(x.end(),o){o<<'{';int i=2;for(auto e:x)o<<(", ")+i<<e,i=0;return o<<'}';}
+#define LOG(x...)cerr<<"["#x"]: ",[](auto...$){((cerr<<$<<"; "),...)<<endl;}(x);
+#else
+#define LOG(x...)(void)0
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x)  (x).begin(),(x).end()
+#define endl    '\n'
+#define size(x)  x.size()
+
+const ll INF = 9223372036854775806;
+const ll MAX_N = 1e9+1;
+const ll MOD = 1e9+7; // 998244353
+
+void solveTestCase() {
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+
+    vector<int> B(M);
+    for (int i = 0; i < M; ++i) {
+        cin >> B[i];
+    }
+
+    vector<int> DP(M, 0);
+
+    for (int i = 0; i < N; ++i) {
+        int valueA = A[i];
+        int maxValue = 0;
+        for (int j = 0; j < M; ++j) {
+            int valueB = B[j];
+            int oldDP = DP[j];
+
+            if (valueA == valueB) DP[j] = max(DP[j], maxValue + 1);
+            if (__builtin_popcount(valueA ^ valueB) <= K) maxValue = max(maxValue, oldDP);
+        }
+    }
+
+    LOG(DP);
+
+    int finalAnswer = 0;
+    for (int i = 0; i < M; ++i) finalAnswer = max(finalAnswer, DP[i]);
+    cout << finalAnswer << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+
+    while(T--) {
+        solveTestCase();
+    }
+    return 0;
+}
