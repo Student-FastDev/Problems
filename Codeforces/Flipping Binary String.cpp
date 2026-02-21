62198aec03a93ef2c8a796774da91cc5c68fc005
Dawid Kot <101209411+Student-FastDev@users.noreply.github.com>
2026-02-21 18:33:54 +0100

    Added code.

---
+++ Codeforces/Flipping Binary String.cpp
@@ -0,0 +1,72 @@
+// Catling
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+auto operator<<(auto&o,auto p)->decltype(p.first,o){return o<<'('<<p.first<<", "<<p.second <<')';}
+auto operator<<(auto&o,auto x)->decltype(x.end(),o){o<<'{';int i=2;for(auto e:x)o<<(", ")+i<<e,i=0;return o<<'}';}
+#define LOG(x...)cerr<<"["#x"]: ",[](auto...$){((cerr<<$<<"; "),...)<<endl;}(x);
+#else
+#define LOG(x...)(void)0
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x)  (x).begin(),(x).end()
+#define endl    '\n'
+#define size(x)  x.size()
+
+const ll INF = 9223372036854775806;
+const ll MAX_N = 1e9+1;
+const ll MOD = 1e9+7; // 998244353
+
+void solveTestCase() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    int C = 0;
+    for (char c : S) {
+        if (c == '1') {
+            C++;
+        }
+    }
+
+    if (C % 2 == 0) {
+        cout << C <<endl;
+        for (int i = 0; i < N; i++) {
+            if (S[i] == '1') {
+                cout << i + 1 << " ";
+            }
+        }
+        cout << endl;
+    } 
+    else if ((N - C) % 2 == 1) {
+        cout << N - C << endl;
+        for (int i = 0; i < N; i++) {
+            if (S[i] == '0') {
+                cout << i + 1 << " ";
+            }
+        }
+        cout << endl;
+    } 
+    else {
+        cout << -1 << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        solveTestCase();
+    }
+    return 0;
+}
